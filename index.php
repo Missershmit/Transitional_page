999e367b62b1827ec598f4d27390ecc5f55a8694
Lesha <a.myshkovets@gmail.com>
2015-02-24 20:28:24 +0200

    Some changes about transitional page.

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
-<?='$Hello_Lesha'?>
-<br>
-<?php
-echo 'Hello_World';
-?>
 
+
+<form method="GET" action="http://transitional-page.local/">
+    Введіть Ваше ім'я: <input type="text" name="name">
+    <br>
+    <input type="submit" name="okbutton" value="OK">
+</form>
