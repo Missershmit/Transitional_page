887f2da6fb29971e64751e4efcdc25e31fd87360
Lesha <a.myshkovets@gmail.com>
2015-02-25 22:00:13 +0200

    I've made transition from page "http://transitional-page.local/" to page "http://transitional-page.local/transitional_page.php", with using $_POST method. Also I've added some information about Server (with using $_SERVER) to this page.

--- index.php
+++ index.php
@@ -1,11 +1,26 @@
-<form method="GET" action="http://transitional-page.local/transitional_page.php">
-    Введите имя пользователя: <input type="text" name="name">
-    <br>
+<html>
+<body>
+<?
+$name = isset($_POST['name']) ? htmlspecialchars($_POST['name']) : '';
+$year = isset($_POST['year']) ? htmlspecialchars($_POST['year']) : '';
+/*if (isset($_POST['name'], $_POST['year'])) {
+    if ($_POST['name'] == '') {
+        echo 'Укажите имя!<br>';
+    } else if ($_POST['year'] < 1900 || $_POST['year'] >= 2015) {
+        echo 'Укажите год рождения! Допустимый диапазон значений: 1900..2015 <br>';
+    } else {
+        echo 'Здравствуйте, ' . $name . '!<br>';
+           }
 
-    <input type="submit" name="okbutton" value="OK">
-</form>
-
-<?php
-echo 'Transitional Page';
+    echo '<hr>';
+}*/
 ?>
+<form method="post" action=" transitional_page.php">
 
+    Введите Ваше имя: <input type="text" name="name" value="<?=$name?>">
+    <br>
+    Введите Ваш год рождения: <input type="text" name="year" value="<?=$year?>">
+    <input type="submit" value="">
+</form>
+</body>
+</html>
--- transitional_page.php
+++ transitional_page.php
@@ -1,3 +1,12 @@
 <?php
-echo 'Transitional Page';
+    $title = 'transitional page';
+    echo ucwords($title) . "<br />\n"; "<br />\n";
+    $name = isset($_POST['name']) ? htmlspecialchars($_POST['name']) : '';
+    $year = isset($_POST['year']) ? htmlspecialchars($_POST['year']) : '';
+
+    echo "Имя: $name <br />\n";
+    echo "Год: $year <br />\n";
+
+    echo $_SERVER["DOCUMENT_ROOT"] . "<br />\n";
+    echo $_SERVER["SERVER_ADDR"] . "<br />\n";
 ?>
